ba3f4e08c9e2f3deb9338c1cd3a7fd170de66de3
softwebdev05 <softwebdev05@gmail.com>
2022-05-05 12:14:45 +0300

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import 'materialize-css/dist/css/materialize.min.css';
 import "./App.css";
+import BmiForm from "./BmiForm";
 
 function App() {
+
+  function handleChange(data) {
+    console.log(data);
+  }
   return (
-    <div>
-      <h1>App</h1>
+       <div className='container'>
+      <div className='row center'>
+        <h1 className='white-text'> BMI Tracker </h1>
+      </div>
+      <div className='row'>
+        <div className='col m12 s12'>
+          <BmiForm onChange={handleChange} />
+        </div>
+      </div>
     </div>
   );
 }
