047aea36677594f4c05e15457f12384d01f56d9f
softwebdev05 <softwebdev05@gmail.com>
2022-05-05 12:21:16 +0300

    converted from js to jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import 'materialize-css/dist/css/materialize.min.css';
+import "./App.css";
+import { v4 as uuidv4 } from 'uuid';
+import BmiForm from "./BmiForm";
+
+function App() {
+  const [state, setState] = useState([]);
+  function handleChange(newData) {
+    const { height, weight } = newData;
+    const heightInM = height / 100;
+    newData.bmi = (weight / (heightInM * heightInM)).toFixed(2);
+    newData.id = uuidv4();
+    let newState = [...state, newData];
+    let len = newState.length;
+    if (len > 7) newState = newState.slice(1, len);
+    setState(newState);
+  }
+
+  function handleDelete(id) {
+    setState(prevValue => {
+      return prevValue.filter(info => info.id !== id);
+    });
+  }
+
+  return (
+    <div className='container'>
+      <div className='row center'>
+        <h1 className='white-text'> BMI Tracker </h1>
+      </div>
+      <div className='row'>
+        <div className='col m12 s12'>
+          <BmiForm onChange={handleChange} />
+          <div>
+            <div className='row center'>
+              <h4 className='white-text'>7 Day Data</h4>
+            </div>
+            <div className='data-container row'>
+              {state.length > 0 ? (
+                state.map(info => {
+                  return (
+                    <Info 
+                      key={info.id}
+                      id={info.id}
+                      weight={info.weight}
+                      height={info.height}
+                      date={info.date}
+                      bmi={info.bmi}
+                      deleteCard={handleDelete}
+                    />
+                  );
+                })
+              ) : (
+                <div className='center white-text'>No log found</div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;  
